7d2ced2fdd02999ad56d4415b09430416b3c605e
Ometu Emmanuel <ometuemmanuel967@gmail.com>
2026-06-23 09:36:44 +0100

    fix server

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// HOME ROUTE
-app.get("/", (req, res) => {
-  res.send("🏆 GamePunter API is LIVE");
-});
-
-// SAMPLE FREE TIPS
 app.get("/api/tips", (req, res) => {
   res.json([
     {
@@ -31,6 +25,4 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
-});
-  console.log("Server running on port 3000");
 });
